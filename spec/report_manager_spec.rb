5e58afdc944e66dc0e58e6f777aa8c0de1bb9af7
Ladislav Martincik <ladislav.martincik@gmail.com>
2012-09-28 20:11:54 +0200

    Fixing build

--- spec/report_manager_spec.rb
+++ spec/report_manager_spec.rb
@@ -17,21 +17,18 @@ describe Crashdesk::ReportManager do
     end
 
     it "and with Logger should print it with PP" do
-      Crashdesk.should_receive(:log).with({:name => 1, :string => 2})
-      PP.should_receive(:pp).with(name: 1, string: 2)
+      Crashdesk.should_receive(:log).with("{:name=>1, :string=>2}")
       @report_manager = Crashdesk::ReportManager.new([:logger])
       @report_manager.process(@crashlog)
     end
 
     it "and with Foo logger should return echo and also with logger call PP" do
       @report_manager = Crashdesk::ReportManager.new([:logger, :test])
-      PP.should_receive(:pp).with(name: 1, string: 2)
       @report_manager.process(@crashlog).should eq([nil, {name:1,string:2}])
     end
 
     it "must accept also constants as argument" do
       @report_manager = Crashdesk::ReportManager.new([:logger, ::Crashdesk::Reporters::Test.new])
-      PP.should_receive(:pp).with(name: 1, string: 2)
       @report_manager.process(@crashlog).should eq([nil, {name:1,string:2}])
     end
 
